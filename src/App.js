29c178f6e2b27aa4a2c6dc91c8f25897d898bded
Aritra Saha <aritrasaha01@gmail.com>
2022-04-27 10:54:40 -0400

    remove comments

--- src/App.js
+++ src/App.js
@@ -3,23 +3,6 @@ import Timeline from './components/Timeline/Timeline';
 import Header from './components/Header/Header';
 
 function App() {
-
-  // const [data, setData] = useState([]);
-
-  // const apiKey = "https://api.nasa.gov/planetary/apod?api_key=d4catWkayonYRsvEhfcV7ipRbA6vTJ3aK7bW4ozz&count=15&thumbs=TRUE";
-  
-  // // api key: d4catWkayonYRsvEhfcV7ipRbA6vTJ3aK7bW4ozz&count=30&thumbs=TRUE
-
-  // useEffect(() => {
-  //     fetch(apiKey)
-  //         .then(res => res.json())
-  //         .then(
-  //             (result) => {
-  //                 setData(result);
-  //             }
-  //         )
-  // }, [])
-  
   return (
     <div className='App'>
       <Header/>
